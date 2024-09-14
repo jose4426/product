add30409a1b6e238dad18ea3670eaeee37641511
Jose Gonzalez <gonzalezj231@gmail.com>
2024-09-14 08:52:46 -0400

    debug

--- src/main/java/com/example/product/controller/ScrapingController.java
+++ src/main/java/com/example/product/controller/ScrapingController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/cambio")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class ScrapingController {
     @Autowired
     private ScraperService scrapingService;
